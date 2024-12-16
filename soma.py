57bfacef923af68dc25e0097903b9f0054bcb58a
Bruno Ramos <fazerolibr@gmail.com>
2024-12-16 10:22:21 +0000

    pseudocodigo e Python

---
+++ soma.py
@@ -0,0 +1,11 @@
+a = 0
+b = 0
+
+soma = 0
+a = int(input("Introduza um numero inteiro: "))
+
+b = int(input("introduza um numero inteiro: "))
+
+soma = a + b
+
+print("o resultado da soma", a, " + " , b, " = ", soma)
